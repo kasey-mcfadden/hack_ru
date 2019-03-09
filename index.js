f92949b4881c73cc692b87a82874aa651270391e
Kasey McFadden <kaseysmcfadden@gmail.com>
2019-03-09 15:43:01 -0500

    simple bar chart

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 // javascript
 
-d3.select();
-d3.selectAll();
+var dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
 
-d3.select('h1').style('color', 'red')
-.attr('class', 'heading')
-.text('Updated h1 tag');
+var svgWidth = 500, svgHeight = 300, barPadding = 5;
+var barWidth = (svgWidth / dataset.length);
 
-d3.select('body').append('p').text('First Paragraph');
-d3.select('body').append('p').text('Second Paragraph');
-d3.select('body').append('p').text('Third Paragraph');
+
+var svg = d3.select('svg')
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
+    
+var barChart = svg.selectAll("rect")
+    .data(dataset)
+    .enter()
+    .append("rect")
+    .attr("y", function(d) {
+         return svgHeight - d 
+    })
+    .attr("height", function(d) { 
+        return d; 
+    })
+    .attr("width", barWidth - barPadding)
+    .attr("transform", function (d, i) {
+        var translate = [barWidth * i, 0]; 
+        return "translate("+ translate +")";
+    });
