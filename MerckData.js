48a0994845c5a011a28a9d94a6335eae9d5062c2
Kasey McFadden <kaseysmcfadden@gmail.com>
2019-03-10 09:38:58 -0400

    UI update

--- MerckData.js
+++ MerckData.js
@@ -1,4 +1,5 @@
 var addressPoints = [
-[-5.45260496, 5.50270993, "Merck1"],
-[40.502712, -74.452496, "Merck2"],
+[-5.45260496, 5.50270993, "globalLocationNumber 0300060000034 - Received (Warehouse)"],
+[40.502712, -74.452496, "globalLocationNumber 0300060000034 - Received (Warehouse)"],
+[0.0, 0.0, "globalLocationNumber 0300060000034 - Received (Warehouse)"]
 ];
