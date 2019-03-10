32a4e0d287e58a6a3210dcc622ec202980595022
Kasey McFadden <kaseysmcfadden@gmail.com>
2019-03-10 10:36:07 -0400

    update

--- citiesConvert.py
+++ citiesConvert.py
@@ -8,16 +8,25 @@ output = "var addressPoints = [\n"
 with open('largest_cities.json') as f:
     data = json.load(f)
 
+packStat = ["0 - Unpacked", "1 - Packed"]
 logStat = ["0 - Not Shipped", "1 - In Transit", "2 - Received"]
+table = []
 
 for x in data:
 	lat = json.dumps(x["fields"]["coordinates"][0])
 	lon = json.dumps(x["fields"]["coordinates"][1])
-	output += ("[" + lat + ", " + lon + ", " + json.dumps(x["fields"]["city"]) + " Status: " + logStat[randint(0, 2)] + " Expiry: 2021-07-07T00:00:00.000Z" + "],\n")
-
+	stat = logStat[randint(0, 2)]
+	pack = packStat[randint(0, 1)]
+	output += ("[" + lat + ", " + lon + ", " + "\"" + x["fields"]["city"] + " | Status: " + stat + " | Expiry: 2021-07-07T00:00:00.000Z" + "\"],\n")
+	table.append([x["fields"]["city"], lat, lon, x["recordid"], pack, stat])
+	
 
 output = output[:-2]
 output += '\n];'
+
+df = pandas.DataFrame(table, columns=["city", "latitude", "longitude", "globalLocationNumber", "packStatus", "logisticStatus"])
+df.to_html("generatedTable.html", classes="table")
+
 
 sys.stdout = open('OurData.js', 'w')
 print(output)
